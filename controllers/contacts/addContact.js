f142b5e6a1c9aa19b563484cbc05c94016d0aeb0
Hi-tei-hi-me <lethargium@gmail.com>
2023-03-24 23:25:51 +0200

    Controllers UPD

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,25 +1,5 @@
 const contactsActions = require("../../models/contacts");
 
-<<<<<<< Updated upstream
-const addContact = async (req, res, next) => {
-  try {
-    const { error } = contactSchema.validate(req.body);
-    if (error) {
-      throw HttpError(404, error.message);
-    }
-    const addedContact = await contactsActions.addContact({ name, email, phone });
-    res.json({
-      status: "success",
-      code: 201,
-      message: "Contact added",
-      data: {
-        addedContact,
-      },
-    });
-  } catch (err) {
-    next(err);
-  }
-=======
 const addContact = async (req, res) => {
   const addedContact = await contactsActions.addContact({ name, email, phone });
   res.json({
@@ -30,7 +10,6 @@ const addContact = async (req, res) => {
       addedContact,
     },
   });
->>>>>>> Stashed changes
 };
 
 module.exports = addContact;
--- controllers/contacts/updateContactById.js
+++ controllers/contacts/updateContactById.js
@@ -1,30 +1,6 @@
 const contactsActions = require("../../models/contacts");
 const { HttpError } = require("../../utils");
 
-<<<<<<< Updated upstream
-const updateContactById = async (req, res, next) => {
-  try {
-    const { error } = contactSchema.validate(req.body);
-    if (error) {
-      throw HttpError(404, error.message);
-    }
-    const { contactId } = req.params;
-    const updatedContact = await contactsActions.updateContact(contactId, req.body);
-    if (!updatedContact) {
-      throw HttpError(404, "Not found");
-    }
-    res.json({
-      status: "success",
-      code: 200,
-      message: "Contact updated",
-      data: {
-        updatedContact,
-      },
-    });
-  } catch (error) {
-    next(err);
-  }
-=======
 const updateContactById = async (req, res) => {
   const { contactId } = req.params;
   const updatedContact = await contactsActions.updateContact(contactId, req.body);
@@ -39,7 +15,6 @@ const updateContactById = async (req, res) => {
       updatedContact,
     },
   });
->>>>>>> Stashed changes
 };
 
 module.exports = updateContactById;
