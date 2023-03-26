65a7556dc07fa38ba2468ff6e1f884af7222822b
Hi-tei-hi-me <lethargium@gmail.com>
2023-03-27 01:16:43 +0300

    Miscellaneous

--- middlewares/userAuthentication.js
+++ middlewares/userAuthentication.js
@@ -9,18 +9,18 @@ const authentication = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
   if (bearer !== "Bearer" || !token) {
-    next(RequestError(401, "Not authorized"));
+    next(RequestError(401));
   }
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
     if (!user || !user.token || user.token !== token) {
-      next(RequestError(401, "Not authorized"));
+      next(RequestError(401));
     }
     req.user = user;
     next();
   } catch {
-    next(RequestError(401), "Not authorized");
+    next(RequestError(401));
   }
 };
 
