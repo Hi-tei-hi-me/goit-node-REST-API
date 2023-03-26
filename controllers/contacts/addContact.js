fb85a003550120e0d7372d5521e9d537f4826323
Hi-tei-hi-me <lethargium@gmail.com>
2023-03-27 00:10:57 +0300

    Controllers UPD

--- controllers/contacts/addContact.js
+++ controllers/contacts/addContact.js
@@ -1,7 +1,8 @@
 const { Contact } = require("../../models/contact.js");
 
 const addContact = async (req, res) => {
-  const addedContact = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const addedContact = await Contact.create({ ...req.body, owner });
   res.status(201).json({
     status: "success",
     code: 201,
