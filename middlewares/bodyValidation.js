a5f4211975df90d739e7dabd0db2787e8c55f35c
Hi-tei-hi-me <lethargium@gmail.com>
2023-03-27 00:11:52 +0300

    Middlewares UPD

--- middlewares/bodyValidation.js
+++ middlewares/bodyValidation.js
@@ -4,7 +4,7 @@ const bodyValidation = (schema, message) => {
   return (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
-      next(HttpError(400, error.message));
+      next(HttpError(400, (error.message = message || error.details[0].message)));
     }
     next();
   };
